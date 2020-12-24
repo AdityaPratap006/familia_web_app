1a2cf04a04d038423270c1e75619d943a01a9576
Aditya Pratap <adityapratap7344@gmail.com>
2020-12-25 02:40:59 +0530

    created tabs, added grid

--- src/screens/InvitesScreen/index.tsx
+++ src/screens/InvitesScreen/index.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import { InvitesScreenContent } from './style';
+import React, { useState } from 'react';
+import { InvitesGrid, InvitesScreenContent, InvitesTab, InvitesTabsHeader } from './style';
 import Screen from '../../components/ScreenComponents/Screen';
+import Card from '../../components/Card';
+
+enum InviteTab {
+    SENT = 'SENT',
+    RECEIVED = 'RECEIVED',
+}
 
 const InvitesScreen: React.FC = () => {
+    const [activeTab, setActiveTab] = useState<InviteTab>(InviteTab.RECEIVED);
+
+    const handleTabChange = (tab: InviteTab) => {
+        setActiveTab(tab);
+    }
+
     return (
         <Screen
             title="Invites"
@@ -10,7 +22,30 @@ const InvitesScreen: React.FC = () => {
             withoutBottomAppBar
         >
             <InvitesScreenContent>
-                Invite
+                <InvitesTabsHeader>
+                    <InvitesTab
+                        type="button"
+                        className={`${activeTab === InviteTab.RECEIVED && 'active'}`}
+                        onClick={() => handleTabChange(InviteTab.RECEIVED)}
+                    >
+                        Received
+                    </InvitesTab>
+                    <InvitesTab
+                        type="button"
+                        className={`${activeTab === InviteTab.SENT && 'active'}`}
+                        onClick={() => handleTabChange(InviteTab.SENT)}
+                    >
+                        Sent
+                    </InvitesTab>
+                </InvitesTabsHeader>
+                <InvitesGrid>
+                    <Card>
+                        afafaa
+                        afafsef
+                        aefaesfaef
+                        aefaesfes
+                    </Card>
+                </InvitesGrid>
             </InvitesScreenContent>
         </Screen>
     );
--- src/screens/InvitesScreen/style.tsx
+++ src/screens/InvitesScreen/style.tsx
@@ -1,5 +1,64 @@
 import styled from 'styled-components';
+import { ScreenSize } from '../../utils/screenSizes';
 
 export const InvitesScreenContent = styled.div`
     width: 100%;
 `;
+
+export const InvitesContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+`;
+
+export const InvitesTabsHeader = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    border-bottom: 1px solid ${props => props.theme.primary};
+`;
+
+export const InvitesTab = styled.button`
+    margin: 0;
+    padding: 0.8rem 2rem;
+    outline: none;
+    text-decoration: none;
+    -webkit-tap-highlight-color: transparent;
+    background: ${props => props.theme.background};
+    font-size: 1rem;
+    font-weight: bold;
+    color: ${props => props.theme.primary};
+    border: none;
+    font-family: inherit;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+
+    &.active {
+        background: ${props => props.theme.primary};
+        color: #fff;
+    }
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export const InvitesGrid = styled.div`
+    padding-top: 1rem;
+    width: 100%;
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: repeat(3, 1fr);
+
+    @media (max-width: ${ScreenSize.SM_MAX}) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media (max-width: ${ScreenSize.XS_MAX}) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`;
+
