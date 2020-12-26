7e127256aeda28da2b7d7ca5535c1cf017f51598
Aditya Pratap <adityapratap7344@gmail.com>
2020-12-26 18:42:25 +0530

    rendering add member card only if current user is the creator of the current family

--- src/components/FamilyMemberComponents/AddMemberCard/index.tsx
+++ src/components/FamilyMemberComponents/AddMemberCard/index.tsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { RiUserAddFill } from 'react-icons/ri';
+import { FamilyContext } from '../../../contexts/family.context';
+import { UserProfileContext } from '../../../contexts/userProfile.context';
 import SearchUserModal from '../SearchUserModal';
 import { StyledCard, StyledIconContainer, StyledText } from './style';
 
 const AddMemberCard: React.FC = () => {
+    const { profile } = useContext(UserProfileContext);
+    const { currentFamily } = useContext(FamilyContext);
     const [showSearchUserModal, setShowSearchUserModal] = useState(false);
+
+    if (!profile || !currentFamily) {
+        return null;
+    }
+
+    if (profile._id !== currentFamily.creator._id) {
+        return null;
+    }
 
     const openSearchUserModal = () => {
         setShowSearchUserModal(true);
